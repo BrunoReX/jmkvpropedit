b423b395c257fc6cf6066c2fadafa2ed5b650b93
Bruno Barbieri <brunorex@gmail.com>
2012-01-11 20:59:32 -0200

    Added BSD license header (whatever)

--- trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
+++ trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
@@ -1,3 +1,16 @@
+/* 
+ * Copyright (c) 2012, Bruno Barbieri
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
 package com.googlecode.jmkvpropedit;
 
 import java.awt.*;
@@ -1868,9 +1881,14 @@ public class JMkvpropedit {
 				if (exePath != null) {
 					File exeFile = new File(exePath);
 					if (exeFile.exists()) {
-						txtMkvPropExe.setText(exePath);
-						cbMkvPropExeDef.setSelected(false);
-						cbMkvPropExeDef.setEnabled(true);
+						if (exeFile.toString().equals("mkvpropedit.exe")) {
+							cbMkvPropExeDef.setSelected(true);
+							cbMkvPropExeDef.setEnabled(false);
+						} else {
+							txtMkvPropExe.setText(exePath);
+							cbMkvPropExeDef.setSelected(false);
+							cbMkvPropExeDef.setEnabled(true);
+						}
 					} else {
 						txtMkvPropExe.setText("mvkpropedit.exe");
 						cbMkvPropExeDef.setSelected(true);
@@ -1945,7 +1963,7 @@ public class JMkvpropedit {
 	private String getMkvPropExeFromReg() {
 		String exePath = null;
 		
-		try {
+		try {			
 			exePath = WinRegistry.readString(WinRegistry.HKEY_LOCAL_MACHINE,
 					"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\MKVtoolnix",
 					"UninstallString");
--- trunk/src/com/googlecode/jmkvpropedit/WinRegistry.java
+++ trunk/src/com/googlecode/jmkvpropedit/WinRegistry.java
@@ -1,3 +1,16 @@
+/* 
+ * Copyright (c) 2012, Bruno Barbieri
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
 package com.googlecode.jmkvpropedit;
 
 import java.lang.reflect.InvocationTargetException;
@@ -7,6 +20,12 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.prefs.Preferences;
+
+/*
+ * Original code by Réal Gagnon
+ * Source: http://www.rgagnon.com/javadetails/java-0630.html
+ * 
+ */
 
 public class WinRegistry {
   public static final int HKEY_CURRENT_USER = 0x80000001;
