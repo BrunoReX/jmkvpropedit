5a2a00e0b80934ed084ae00cbbc26652f8f2b717
Bruno Barbieri <brunorex@gmail.com>
2012-06-27 22:05:12 -0300

    Changed right-click menu method to generic text component, this way two identical methods aren't needed.

--- trunk/src/com/googlecode/jmkvpropedit/Utils.java
+++ trunk/src/com/googlecode/jmkvpropedit/Utils.java
@@ -122,7 +122,7 @@ public class Utils {
 	
 	/* Start of right-click menu code */
 	
-	private static void showRCMenu(JTextArea text, MouseEvent e) {
+	private static void showRCMenu(JTextComponent text, MouseEvent e) {
 		JPopupMenu rightClickMenu = new JPopupMenu();
 		JMenuItem copyMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.copyAction));
 		JMenuItem cutMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.cutAction));
@@ -163,60 +163,7 @@ public class Utils {
 		rightClickMenu.show(text, e.getX(), e.getY());
 	}
 	
-	public static void addRCMenuMouseListener(final JTextArea text) {
-		text.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-				if (e.isMetaDown() && text.isEnabled()) {
-					text.requestFocus();
-					showRCMenu(text, e);
-				}
-			}
-		});
-	}
-
-	private static void showRCMenu(JTextField text, MouseEvent e) {
-		JPopupMenu rightClickMenu = new JPopupMenu();
-		JMenuItem copyMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.copyAction));
-		JMenuItem cutMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.cutAction));
-		JMenuItem pasteMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.pasteAction));
-		JMenuItem selectAllMenuItem = new JMenuItem(text.getActionMap().get(DefaultEditorKit.selectAllAction));
-		
-		copyMenuItem.setText("Copy");
-		cutMenuItem.setText("Cut");
-		pasteMenuItem.setText("Paste");
-		selectAllMenuItem.setText("Select All");
-
-		rightClickMenu.add(copyMenuItem);
-		rightClickMenu.add(cutMenuItem);
-		rightClickMenu.add(pasteMenuItem);
-		rightClickMenu.addSeparator();
-		rightClickMenu.add(selectAllMenuItem);
-		
-		if (text.getText().isEmpty()) {
-			copyMenuItem.setEnabled(false);
-			selectAllMenuItem.setEnabled(false);
-			cutMenuItem.setEnabled(false);
-		}
-		
-		if (text.getSelectionStart() == text.getSelectionEnd()) {
-			copyMenuItem.setEnabled(false);
-			cutMenuItem.setEnabled(false);
-		}
-		
-		if ((text.getSelectionStart()+text.getSelectionEnd()) == text.getText().length()) {
-			selectAllMenuItem.setEnabled(false);
-		}
-		
-		if (!text.isEditable()) {
-			cutMenuItem.setEnabled(false);
-			pasteMenuItem.setEnabled(false);
-		}
-		
-		rightClickMenu.show(text, e.getX(), e.getY());
-	}
-	
-	public static void addRCMenuMouseListener(final JTextField text) {
+	public static void addRCMenuMouseListener(final JTextComponent text) {
 		text.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
