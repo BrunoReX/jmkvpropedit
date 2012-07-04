e14d8fd5d2f57c1301fd0f057704ddd88cb47e73
Bruno Barbieri <brunorex@gmail.com>
2012-07-04 11:25:19 -0300

    Bumped version to 1.1.1.

--- trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
+++ trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
@@ -41,7 +41,7 @@ import org.ini4j.*;
 
 public class JMkvpropedit {
 	
-	private static final String VERSION_NUMBER = "1.1";
+	private static final String VERSION_NUMBER = "1.1.1";
 	private static final int MAX_STREAMS = 30;
 	private static String[] argsArray;
 	
