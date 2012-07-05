0f37663d2ef6e56637aa410c3def65541bc15244
Bruno Barbieri <brunorex@gmail.com>
2012-07-05 10:12:12 -0300

    Remove unused import

--- trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
+++ trunk/src/com/googlecode/jmkvpropedit/JMkvpropedit.java
@@ -29,7 +29,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
