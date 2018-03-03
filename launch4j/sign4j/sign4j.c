3e6b45ab11f42fb231c29ebe4b0f4488ccc7b69a
Bruno Barbieri <brunorex@gmail.com>
2018-03-03 08:59:57 -0500

    Updated some libraries
    - ini4j from v0.5.2 to v0.5.4
    - Apache Commons IO from v2.4 to v2.6
    - Launch4j from v3.4 to v3.11

--- launch4j/sign4j/sign4j.c
+++ launch4j/sign4j/sign4j.c
@@ -1,40 +1,45 @@
 /*
 	sign4j.c: a simple utility to sign executables created by Launch4j
 
-	Copyright (c) 2012 [TODO]
-
+	Copyright (c) 2012 Servoy
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without modification,
 	are permitted provided that the following conditions are met:
-
-	    * Redistributions of source code must retain the above copyright notice,
-	      this list of conditions and the following disclaimer.
-	    * Redistributions in binary form must reproduce the above copyright notice,
-	      this list of conditions and the following disclaimer in the documentation
-	      and/or other materials provided with the distribution.
-	    * Neither the name of the Launch4j nor the names of its contributors
-	      may be used to endorse or promote products derived from this software without
-	      specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+	
+	3. Neither the name of the copyright holder nor the names of its contributors
+	   may be used to endorse or promote products derived from this software without
+	   specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+	AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#ifdef _WIN32
 #include <io.h>
 #include <fcntl.h>
+#else
+#include <sys/io.h>
+#include <sys/fcntl.h>
+#endif
 #include <sys/types.h>
 #include <sys/stat.h>
 
@@ -44,6 +49,14 @@
 #define SWAP_BLOCK_SIZE    (4 * 1024 * 1024)
 #define TEST_FILE_NAME     "sign4j_temporary.exe"
 #define SIGN4J_VERSION     "3.0"
+
+#ifndef _WIN32
+#define O_BINARY 0
+#define _O_SHORT_LIVED 0
+#define _S_IREAD S_IREAD
+#define _S_IWRITE S_IWRITE
+#define stricmp strcasecmp
+#endif
 
 typedef unsigned char byte;
 
@@ -127,7 +140,12 @@ int main (int argc, char* argv[])
          trg = outf;
       close (fd);
 
+#ifdef _WIN32
       strcpy (command,  "\" ");
+#else
+      strcpy (command, "");
+#endif
+
       for (i = j; i < argc; i++)
       {
          p = (argv[i] == outf ? trg : argv[i]);
@@ -139,9 +157,18 @@ int main (int argc, char* argv[])
             strcat (command, "\"");
          strcat (command, " ");
       }
+
+#ifdef _WIN32
       strcat (command, "\"");
+#endif
+
       if (! vrb)
+#ifdef _WIN32
          strcat (command, " > NUL");
+#else
+         strcat (command, " > /dev/null");
+#endif
+
       system (command);
 
       if ((td = open (trg, O_RDONLY | O_BINARY)) < 0)
@@ -168,7 +195,12 @@ int main (int argc, char* argv[])
       printf ("You don't need sign4j to sign this file\n");
    }
 
+#ifdef _WIN32
    strcpy (command,  "\" ");
+#else
+   strcpy (command, "");
+#endif
+
    for (i = j; i < argc; i++)
    {
       p = argv[i];
@@ -180,7 +212,11 @@ int main (int argc, char* argv[])
          strcat (command, "\"");
       strcat (command, " ");
    }
+
+#ifdef _WIN32
    strcat (command, "\"");
+#endif
+
    return system (command);
 }
 
